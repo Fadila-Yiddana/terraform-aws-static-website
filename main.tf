c0710623c43909ee39b743e015e2a44a155703a6
Fadila Yiddana <dilayidd10@gmail.com>
2026-06-18 18:45:14 +0000

    Add S3 bucket website configuration

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
