f7e3553cb4e7242bf5d9c5cd0b1bc92ccc6cc0dd
Fadila Yiddana <dilayidd10@gmail.com>
2026-06-18 18:51:32 +0000

    Add CloudFront distribution for S3 website

--- main.tf
+++ main.tf
@@ -9,3 +9,38 @@ resource "aws_s3_bucket_website_configuration" "website" {
     suffix = "index.html"
   }
 }
+
+resource "aws_cloudfront_distribution" "website_cdn" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.website.website_endpoint
+    origin_id   = "S3Website"
+  }
+
+  enabled = true
+
+  default_cache_behavior {
+    target_origin_id       = "S3Website"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
